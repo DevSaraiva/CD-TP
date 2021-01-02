c2dd02cca3490836d9520e09ecf2832af6d3840f
Guilherme Bretão Monteiro <61871927+rushmetra@users.noreply.github.com>
2021-01-02 20:58:36 -0100

    Add files via upload

--- Parte_A/moduloA.c
+++ Parte_A/moduloA.c
@@ -5,6 +5,7 @@
 #include "buffer.h"
 #include <math.h>
 #include "fsize.h"
+#include <time.h>
 
 int excecao(char* arr, int i){
     if(arr[i] == '{' && arr[i+1] == '0' && arr[i+2] == '}') return 1;
@@ -56,14 +57,14 @@ int conta_simbolos(char* arr){
 
 int compressao(Buffer origem, int simb_comprimido){   //, char* original)
     int simb_original = conta_simbolos(origem.array) - 1; 
-    //printf("Simbolos original: %d\n",simb_original);
-    //printf("Simbolos comprimido: %d\n",simb_comprimido);
+    printf("Simbolos original: %d\n",simb_original);
+    printf("Simbolos comprimido: %d\n",simb_comprimido);
 
     int res = (100*(simb_original - simb_comprimido)) / simb_original;
     if(((100*(simb_original - simb_comprimido)) % simb_original) >= 0.5){
         res++;
     }
-    //printf("Compressao de %d%%\n",res);
+    printf("Compressao de %d%%\n",res);
     return res;
 }
 
@@ -92,14 +93,11 @@ int calcula_n_rep (char a[],int index){
 
 void freq(Buffer bloco,int reps[],int i_block,int block_size,int n_blocks,int total) {
      
-     //int elems[];
-    //int visited = -1;
        int i = (i_block-1)*block_size;
        int fim ;
        if (i_block<n_blocks) fim = i_block*block_size;
        else if (i_block==n_blocks) fim = total;
        while(i < fim) {
-        //printf("%c ",bloco.array[i]);
         char n;
         char n1;
         char n2;
@@ -155,30 +153,15 @@ void freq(Buffer bloco,int reps[],int i_block,int block_size,int n_blocks,int to
                     c2a = bloco.array[i];
                     count += x;
                     reps[c2a] += count;
-                    i += 8; //i += 9;
+                    i += 8; 
                 } 
-                //in = calcula_n_rep(bloco.array,7);
-                //i += 6;
-
-                //c2a = bloco.array[i];
-                //count += in;
-                //reps[c2a] += count;
-
-                //i += 6; //i += 7;
             } 
          } else {
              c2a = bloco.array[i];
-             //count++;
              reps[c2a]++;
         }
         i++;
-        
     } 
-    //int a=0;
-    //while(a<256){
-        //printf("%d ;",reps[a]);
-      //  a++;
-    //}
 }
 
 
@@ -203,7 +186,6 @@ void freqBlock(Buffer *final, int reps[]){
         insertBuffer (final,';');
         i++;
     }
-    //printBuff(final);
 }
 
 // Função que escreve a codificação no ficheiro .freq
@@ -221,15 +203,15 @@ void write_file_buffer_ParteA(Buffer * final, char fileName[]){
     name[i] = fileName[i];
     i++;
   }
-  char termination[] = ".freq";
+  char termination[40] = ".freq";
   strcat(name,termination);
   FILE * file = fopen(name,"w");
   fprintBuff(final,file);
 }
 
-void rle_to_freq(char filename[]){
+void rle_to_freq(char filename[], unsigned long block_size){
     //char filename[] = "aaa.txt.rle";
-     FILE* source = fopen(filename, "rb");
+    FILE* source = fopen(filename, "rb");
     Buffer bloco_rle;
     initBuffer(&bloco_rle,256); //FIXME tamanho do bloco
     char c;
@@ -240,7 +222,6 @@ void rle_to_freq(char filename[]){
     unsigned long long total;
     long long n_blocks;
     long size_of_last_block;
-    unsigned long block_size = 991;
     n_blocks = fsize(source, NULL, &block_size, &size_of_last_block);   // numero blocos
     total = (n_blocks-1) * block_size + size_of_last_block;         // tamanho do ficheiro
     int i_bloco = 1;
@@ -268,13 +249,15 @@ void rle_to_freq(char filename[]){
     write_file_buffer_ParteA(&buffer_final,filename);
 }
 int rle_aux(Buffer bloco_temp, char* filename, int i, int fim) { //FIXME - FAZER A EXCEÇAO dos 5% na main, tendo em conta que e´ so´ pro primeiro BLOCO, temos q fazer um IF 1º bloco_temp e compressao < 5% then APAGAR O FICHEIRO.Rle
-    FILE* destino = fopen("Dest.txt", "wb");         // FIXME - Temos tambem que implentar depois uma maneira de forçar a compressao mesmo que ela sejam menor q 5% conforme o enunciado atraves do interpretador
+
+    FILE* destino = fopen(filename, "wb");
+    //FILE* destino = fopen("Test.txt.rle", "wb");         // FIXME - Temos tambem que implentar depois uma maneira de forçar a compressao mesmo que ela sejam menor q 5% conforme o enunciado atraves do interpretador
     //int i = 0; 
     char atualChar; 
     char proxChar;  
     int n_simbolos = 0;
     int count = 1;
-    
+    int compression;
 while(i < fim) { // aqui antes era i + 1 
      atualChar = bloco_temp.array[i];
      proxChar = bloco_temp.array[i+1];
@@ -322,45 +305,87 @@ while(i < fim) { // aqui antes era i + 1
     return 0;
 }
 
-int rle(Buffer origem,char nome[],unsigned long block_size,int total, int n_blocks) { //(Buffer bloco,int reps[],int i_block,int block_size,int n_blocks,int total)
+int rle(Buffer origem,char filename[],unsigned long block_size,int total, int n_blocks, int* block_compression) { //(Buffer bloco,int reps[],int i_block,int block_size,int n_blocks,int total)
        int i = 0;
        int fim;
+       char terminacao_rle[40] = ".rle";
+       strcat(filename,terminacao_rle);
 
  for(int i_block = 1; i_block <= n_blocks; i_block++){
      i = (i_block-1)*block_size;
      if (i_block<n_blocks) fim = i_block*block_size;
     else fim = total;
-       
-       rle_aux(origem,nome,i,fim);
+       //block_compression[i_block] = rle_aux(origem,filename,i,fim);
+       rle_aux(origem,filename,i,fim);
  }
     return 0;
 }
+//int main(char* filename,unsigned long block_size){ interpretador
 int main(){
+    clock_t begin = clock();
     unsigned long long total;
     long long n_blocks;
     unsigned long size_of_last_block, block_size;
+    int block_compression[n_blocks];
     FILE *fp;
     Buffer origem;
     initBuffer(&origem,256);
     char c;
-    char filename_txt[] = "Test.txt";
+    char filename_txt[40] = "Test.txt";
+
     fp = fopen(filename_txt, "rb");
     while(c != EOF){
         c = fgetc(fp);
         insertBuffer(&origem,c);
     }
-    
+
     block_size = 65536;
     n_blocks = fsize(fp, NULL, &block_size, &size_of_last_block);   // numero blocos
     total = (n_blocks-1) * block_size + size_of_last_block;         // tamanho do ficheiro
-    
-    rle(origem,filename_txt,block_size,total,n_blocks);
+    //int* ptr = 
+    rle(origem,filename_txt,block_size,total,n_blocks,block_compression);
     fclose(fp);
+    //printf("%d", ptr[0]);
 
-
-    //char filename[] = "aaa.txt.rle";
+    //char filename_freq[] = "aaa.txt.rle";
     //rle_to_freq(filename);
-    char filename[] = "Dest.txt";
-    rle_to_freq(filename);
-    
+    char filename_freq[40] = "Test.txt.rle"; // tenho que adicionar .rle ao filename original qnd tiver o interpretador 
+    rle_to_freq(filename_freq,block_size);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    double time_spent_ms = time_spent * 1000;
+    printf("\nRodrigo Rodrigues (a93201) & Rui Monteiro (a93179), MIEI/CD, 1-jan-2021\n");
+    printf("Módulo: f (cálculo das frequências dos símbolos)\n");
+    printf("Número de blocos: %lld\n",n_blocks);
+    if(n_blocks == 1) {
+        printf("Tamanho dos blocos analisados no ficheiro original:_%lu\n", size_of_last_block);
+    } else {
+    printf("Tamanho dos blocos analisados no ficheiro original: %lu/%lu\n",block_size,size_of_last_block);
+    }
+    int sum = 0;
+    for(int j = 0; j < n_blocks; j++) {
+    //    sum += ptr[j];
+    }
+    int compressao_media = sum / n_blocks;
+    printf("Compressão RLE: %d\n", compressao_media);
+    /*
+    FILE* ficheiro_rle;
+    ficheiro_rle = fopen(filename_freq, "rb");
+    int n_blocks_rle = fsize(ficheiro_rle, NULL, &block_size, &size_of_last_block);
+    if(n_blocks_rle == 1) {
+        printf("Tamanho dos blocos analisados no ficheiro RLE:_%lu bytes\n", size_of_last_block);
+    } else {
+    printf("Tamanho dos blocos analisados no ficheiro RLE: %lu bytes /%lu bytes\n",block_size,size_of_last_block);
+    }
+    */
+    printf("Tamanho dos blocos analisados no ficheiro RLE: \n");
+    printf("Tempo de execução do módulo (milissegundos): %f\n",time_spent_ms);
+
+    //char terminacao_rle_print[40] = ".rle";
+    ///strcat(filename_txt,terminacao_rle_print);
+    char terminacao_freq_print[40] = ".freq";
+    strcat(filename_freq,terminacao_freq_print);
+
+    printf("Ficheiros gerados: %s,%s",filename_txt,filename_freq);
 }
